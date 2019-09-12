dd0c726e5bc8af3d0bc27977c1064f49ab72f3ac
Guilherme Leal <lealhugui@gmail.com>
2019-09-12 15:45:14 -0300

    Use of gorm for data access.

--- data/wrappers.go
+++ data/wrappers.go
@@ -1,39 +1,41 @@
 package data
 
 import (
-	"database/sql"
 	"os"
 
-	_ "github.com/mattn/go-sqlite3"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 var GlobalConn SQLiteConn
 
 type SQLiteConn struct {
-	innerConn *sql.DB
+	innerConn *gorm.DB
 }
 
-func (conn SQLiteConn) initInstance(db *sql.DB) {
+func (conn SQLiteConn) initInstance(db *gorm.DB) {
 
 	conn.innerConn = db
 }
 
-func (conn SQLiteConn) Query(sql string) (*sql.Rows, error) {
-
-	return conn.innerConn.Query(sql)
+func (conn SQLiteConn) Query(sql string) *gorm.DB {
+	return conn.innerConn.Raw(sql)
 }
 
-func (conn SQLiteConn) Exec(sql string, args ...interface{}) (int64, error) {
-	stt, errStt := conn.innerConn.Prepare(sql)
-	if errStt != nil {
-		return 0, errStt
-	}
-	result, errExec := stt.Exec(args)
-	if errExec != nil {
-		return 0, errExec
-	}
-	rows, _ := result.RowsAffected()
-	return rows, nil
+func (conn SQLiteConn) Exec(sql string, args ...interface{}) *gorm.DB {
+	/*
+		stt, errStt := conn.innerConn.Prepare(sql)
+		if errStt != nil {
+			return 0, errStt
+		}
+		result, errExec := stt.Exec(args)
+		if errExec != nil {
+			return 0, errExec
+		}
+		rows, _ := result.RowsAffected()
+		return rows, nil
+	*/
+	return conn.innerConn.Exec(sql, args)
 }
 
 func buildConn() {
@@ -47,7 +49,7 @@ func buildConn() {
 		dbPath = "./db.db"
 	}
 
-	db, err := sql.Open("sqlite3", dbPath)
+	db, err := gorm.Open("sqlite3", dbPath)
 	if err != nil {
 
 		panic(err)
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20190910110746-680d30ca3117 // indirect
 	github.com/gin-gonic/gin v1.4.0
+	github.com/jinzhu/gorm v1.9.10
 	github.com/mattn/go-sqlite3 v1.11.0
 	github.com/prometheus/common v0.6.0
 )
